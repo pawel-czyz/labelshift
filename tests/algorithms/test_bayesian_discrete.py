5287dc7df09526b7860ccb41a7194485e2343073
Paweł Czyż <pczyz@protonmail.com>
2022-09-26 22:14:02 +0200

    More docstrings

--- tests/algorithms/test_bayesian_discrete.py
+++ tests/algorithms/test_bayesian_discrete.py
@@ -7,6 +7,8 @@ import labelshift.datasets.discrete_categorical as dc
 
 
 def test_right_values(n_labeled: int = 10_000, n_unlabeled: int = 10_000) -> None:
+    """Test whether the mean over the samples gives the right answer
+    in the limit of large data and little noise."""
     p_y_labeled = np.asarray([0.4, 0.6])
     p_y_unlabeled = np.asarray([0.7, 0.3])
 
