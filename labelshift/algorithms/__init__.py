6a61a1195d0034bba63868ea6d0560fd71336094
Paweł Czyż <pczyz@protonmail.com>
2022-09-27 16:33:36 +0200

    Adjust classify and count to use the new utilities.

--- labelshift/algorithms/__init__.py
+++ labelshift/algorithms/__init__.py
@@ -1,11 +1,3 @@
 """The quantification algorithms submodule.
 Implementations of popular quantification algorithms.
 """
-from labelshift.algorithms.classify_and_count import classify_and_count
-from labelshift.algorithms.expectation_maximization import expectation_maximization
-
-
-__all__ = [
-    "classify_and_count",
-    "expectation_maximization",
-]
--- labelshift/algorithms/classify_and_count.py
+++ labelshift/algorithms/classify_and_count.py
@@ -2,8 +2,10 @@
 import numpy as np
 from numpy.typing import ArrayLike
 
+import labelshift.summary_statistic as summ
 
-def classify_and_count(predictions: ArrayLike, /) -> np.ndarray:
+
+def classify_and_count_from_predictions(predictions: ArrayLike, /) -> np.ndarray:
     """Quantification via counting classifier results.
 
     Args:
@@ -11,20 +13,32 @@ def classify_and_count(predictions: ArrayLike, /) -> np.ndarray:
             Shape (n_samples, n_classes).
 
     Returns:
-        test set prevalences, shape (n_classes,).
+        test set prevalence, shape (n_classes,).
 
     Note:
         This is a very simple approach and may not work well in practice.
     """
     predictions = np.asarray(predictions)
-    n_samples, n_classes = predictions.shape
+    n_classes = predictions.shape[1]
 
     predicted_class = np.argmax(
         predictions, axis=1
     )  # Shape (n_samples,). Entries 0, ..., n_classes-1.
-    # TODO(pawel-czyz): Replace with np.bincount().
-    histogram = np.array(
-        [(predicted_class == i).sum() for i in range(n_classes)], dtype=float
-    )  # Shape (n_classes,).
 
-    return histogram / np.sum(histogram)
+    return classify_and_count_from_labels(n_classes, predicted_class)
+
+
+def classify_and_count_from_labels(n_classes: int, labels: ArrayLike) -> np.ndarray:
+    """Quantification via counting classifier results.
+
+    Args:
+        n_classes: number of different label classes
+        labels: for each example its label.
+          The labels should be from the set {0, ..., n_classes-1}
+
+    Returns:
+        label prevalence vector, shape (n_classes,)
+    """
+    unnormalized = np.asarray(summ.count_values(n_classes, labels), dtype=float)
+
+    return unnormalized / unnormalized.sum()
