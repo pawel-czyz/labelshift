b6e3b0bcb0b1d72f6a3a683b1b1f2e5b67198672
Paweł Czyż <pczyz@protonmail.com>
2022-10-01 12:08:23 +0200

    Add docstring.

---
+++ labelshift/algorithms/ratio_estimator.py
@@ -0,0 +1,6 @@
+"""Implements the ratio estimator described in:
+
+Afonso Fernandes Vaz, Rafael Izbicki, Rafael Bassi Stern;
+Quantification Under Prior Probability Shift: the Ratio Estimator and its Extensions
+Journal of Machine Learning Research, 20(79):1−33, 2019.
+"""
