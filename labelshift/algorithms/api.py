90b3fe5603c83b513c1f11acf0e73e10b13d25c4
Paweł Czyż <pczyz@protonmail.com>
2022-10-03 19:01:24 +0200

    The MAP version of Bayesian quantification

--- labelshift/algorithms/api.py
+++ labelshift/algorithms/api.py
@@ -4,3 +4,14 @@ Use as:
 
 >>> import labelshift.algorithms.api as algo
 """
+from labelshift.algorithms.bayesian_discrete import DiscreteCategoricalMAPEstimator
+from labelshift.algorithms.bbse import BlackBoxShiftEstimator
+from labelshift.algorithms.classify_and_count import ClassifyAndCount
+from labelshift.algorithms.ratio_estimator import InvariantRatioEstimator
+
+__all__ = [
+    "BlackBoxShiftEstimator",
+    "ClassifyAndCount",
+    "DiscreteCategoricalMAPEstimator",
+    "InvariantRatioEstimator",
+]
