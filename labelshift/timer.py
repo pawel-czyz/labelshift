3d39093a189b143618c72f6b110ebe048c8c0a36
Paweł Czyż <pczyz@protonmail.com>
2022-10-03 19:22:54 +0200

    Timer modifications

--- labelshift/timer.py
+++ labelshift/timer.py
@@ -16,12 +16,32 @@ class Timer:
 
     def __init__(self) -> None:
         """Initializes and resets the timer."""
-        self._t0: float = time.time()
+        self._t0: float = self._current_time()
+
+    @staticmethod
+    def _current_time() -> float:
+        """Returns current time."""
+        return time.perf_counter()
 
     def reset(self) -> None:
         """Resets the timer."""
-        self._t0 = time.time()
+        self._t0 = self._current_time()
 
     def check(self) -> float:
         """Returns the elapsed time since the last reset."""
-        return time.time() - self._t0
+        return self._current_time() - self._t0
+
+    def check_and_reset(self) -> float:
+        """Checks the timer and resets it.
+
+        For a given timer:
+        >>> timer = Timer()
+        the expression
+        >>> t = timer.check_and_reset()
+        is equivalent to
+        >>> t = timer.check()
+        >>> timer.reset()
+        """
+        t = self.check()
+        self.reset()
+        return t
