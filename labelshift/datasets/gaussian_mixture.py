fab511f80ffa9ca96603e9863a0df6cbbd495f36
Paweł Czyż <pczyz@protonmail.com>
2022-10-05 13:55:03 +0200

    Add Bayes rule

--- labelshift/datasets/gaussian_mixture.py
+++ labelshift/datasets/gaussian_mixture.py
@@ -20,16 +20,30 @@ class PXGivenY(Protocol):
         """The number of possible labels Y."""
         raise NotImplementedError
 
-    def p_x_cond_y(self, xs: ArrayLike, ys: ArrayLike) -> np.ndarray:
+    def p_x_cond_y_fixed(self, xs: ArrayLike, y: int) -> np.ndarray:
         """Calculates the probability density function p(x|y).
 
         Args:
             xs: vector of points `x`, shape (n_points, dim_x)
-            ys: vector of labels `y`, shape (n_points,)
+            y: label `y`
 
         Returns
             vector of probability density function values p(X=x | Y=y),
-              for each point. Shape (n_points,)
+              for each point `x`. Shape (n_points,)
+        """
+        raise NotImplementedError
+
+    def p_x_cond_y(self, xs: ArrayLike) -> np.ndarray:
+        """Calculates the probability density function p(x|y)
+        for all possible `y`.
+
+        Args:
+            xs: vector of points `x`, shape (n_points, dim_x)
+
+        Returns:
+            probability density function values p(X=x | Y=y),
+              for each point `x` and label `y`.
+              Shape (n_points, number_y).
         """
         raise NotImplementedError
 
@@ -86,12 +100,21 @@ class GaussianMixturePXGivenY(PXGivenY):
         """Dimension of the vector space in which X takes values."""
         return self._dim_x
 
-    def p_x_cond_y(self, xs: ArrayLike, ys: ArrayLike) -> np.ndarray:
-        """The PDF p(X = x | Y = y) for given points."""
-        return np.asarray([self._distributions[y].pdf(x) for x, y in zip(xs, ys)])
+    def p_x_cond_y_fixed(self, xs: ArrayLike, y: int) -> np.ndarray:
+        """See the parent class."""
+        xs = np.asarray(xs)
+        dist = self._distributions[y]
+        return dist.pdf(xs)
+
+    def p_x_cond_y(self, xs: ArrayLike) -> np.ndarray:
+        """See the parent class."""
+        xs = np.asarray(xs)
+        return np.vstack(
+            [self.p_x_cond_y_fixed(xs, y=y) for y in range(self.number_y)]
+        ).T
 
     def sample_p_x_cond_y(self, ys: ArrayLike, rng) -> np.ndarray:
-        """Random samples from distributions associated to the labels."""
+        """See the parent class."""
         rng = np.random.default_rng(rng)
         return np.vstack([self._distributions[y].rvs(random_state=rng) for y in ys])
 
@@ -112,4 +135,15 @@ def posterior_probabilities(
         posterior probability P(Y|X) for each data point.
           Shape (n_points, number_y)
     """
-    raise NotImplementedError
+    p_y = np.asarray(p_y)
+    xs = np.asarray(xs)
+    assert p_y.shape == (p_x_cond_y.number_y,)
+    assert xs.shape[1] == p_x_cond_y.dim_x
+
+    # P(X=x, Y=y) = P(X = x | Y = y) * P(Y=y)
+    # calculated pointwise.
+    # Shape (n_points, number_y)
+    p_x_and_y = p_x_cond_y.p_x_cond_y(xs) * p_y[None, :]
+
+    # We need to normalize p(x, y) to get p(y|x)
+    return p_x_and_y / p_x_and_y.sum(axis=1)[None, :]
